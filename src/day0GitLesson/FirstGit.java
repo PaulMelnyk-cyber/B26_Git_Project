8b7de9c475fb1240ffca55094cd1aef67a9d2d8f
PaulMelnyk <pmelnyk19@gmail.com>
2022-03-26 16:10:29 -0400

    Wrote the code for First class

--- src/day0GitLesson/FirstGit.java
+++ src/day0GitLesson/FirstGit.java
@@ -2,9 +2,12 @@ package day0GitLesson;
 
 public class FirstGit {
 
-    // TODO : works on this tomorrow
-    // add some cool code
+    public static void main(String[] args) {
 
+
+        System.out.println("Java");
+        System.out.println("Working.... ");
+    }
 
     // extra line 1
     // extra line 2
